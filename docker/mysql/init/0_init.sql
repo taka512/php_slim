96a49188a8ee2fddf9ffd03a2efdcad3c76a16f8
taka512 <kapu512@gmail.com>
2020-09-25 19:06:17 +0900

    update mysql 5.5 -> 8.0

--- docker/mysql/init/0_init.sql
+++ docker/mysql/init/0_init.sql
@@ -1,5 +1,6 @@
-create database IF NOT EXISTS sample_slim CHARACTER SET utf8;
-GRANT ALL PRIVILEGES ON sample_slim.* TO slim_user@'%' IDENTIFIED BY 'slim_pass';
-create database IF NOT EXISTS sample_slim_test CHARACTER SET utf8;
-GRANT ALL PRIVILEGES ON sample_slim_test.* TO slim_user@'%' IDENTIFIED BY 'slim_pass';
+CREATE DATABASE IF NOT EXISTS sample_slim CHARACTER SET utf8;
+CREATE DATABASE IF NOT EXISTS sample_slim_test CHARACTER SET utf8;
+CREATE USER IF NOT EXISTS 'slim_user'@'%' identified by 'slim_pass';
+GRANT ALL PRIVILEGES ON sample_slim.* TO slim_user@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON sample_slim_test.* TO slim_user@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
